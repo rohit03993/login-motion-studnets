67b34b9884e0d1ff73b60d5adbec23ffd3b16148
Rohit Pal <rohit03993@gmail.com>
2026-01-12 15:34:38 +0530

    Fix: Register FilesystemServiceProvider via withProviders() before core providers

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,6 +5,11 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
+    ->withProviders([
+        // Register FilesystemServiceProvider FIRST to ensure 'files' binding is available
+        // This fixes "Target class [files] does not exist" error
+        \Illuminate\Filesystem\FilesystemServiceProvider::class,
+    ])
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
