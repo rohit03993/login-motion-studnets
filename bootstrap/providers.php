b2fe2bdef9fcd75651f8b44fb172ddce1828c004
Rohit Pal <rohit03993@gmail.com>
2026-01-12 15:24:36 +0530

    Fix: Register FilesystemServiceProvider early to resolve files binding error

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,5 +1,7 @@
 <?php
 
 return [
+    // Register FilesystemServiceProvider early to fix "Target class [files] does not exist" error
+    Illuminate\Filesystem\FilesystemServiceProvider::class,
     App\Providers\AppServiceProvider::class,
 ];
